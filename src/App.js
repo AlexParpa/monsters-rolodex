20f8555b47437beb540b9d514998cc7c5654232b
AlexParpa <parpalea.alexandru@gmail.com>
2021-08-10 17:32:29 +0300

    rename h1

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
     return (
       <div className="App">
 
-      <h1>Monsters Rolodex TEST</h1>  
+      <h1>Monsters Rolodex</h1>  
       <SearchBox 
         placeholder='search monsters'
         handleChange={this.handleChange}/>
